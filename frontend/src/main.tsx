c7e69bf5a6b59c30285de0d3533fdbd53a6267bb
Wiola Wysopal <115031260+WiolaWysopal@users.noreply.github.com>
2023-11-08 17:00:40 +0100

    9 dodaj framework do routingu (#10)
    * Zainicjalizuj projekt frontendu
    
    * Dockeryzacja frontendu
    
    * Skonfiguruj docker-compose.yml
    
    * Dodaj framework do routingu
    
    ---------
    
    Co-authored-by: srawek <66798628+jackpie2@users.noreply.github.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <div>Strona główna</div>, 
+      },
+      // Tutaj dodać więcej ścieżek jako dzieci
+    ],
+  },
+  // Możesz można dodać więcej głównych ścieżek poza App
+]);
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
+
+// Renderowanie aplikacji z RouterProviderem
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
