5e847fafc41b659db566cb7222480b7ff2344281
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-11-17 17:16:49 +0100

    Wyodrebnij glowny layout

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -4,37 +4,30 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
-import Card from "../components/Card";
 import DashboardButton from "../components/DashboardButton";
-import Clock from "../components/Clock";
 
 export default function Dashboard() {
 	const navigate = useNavigate();
 
 	return (
-		<Card className="h-full w-full text-3xl">
-			<div className="w-full flex justify-end">
-				<Clock className="text-muted text-4xl" />
-			</div>
-			<div className="flex flex-wrap gap-4 justify-center items-center w-full h-full">
-				<DashboardButton
-					onClick={() => {
-						navigate("/menu");
-					}}
-				>
-					<FontAwesomeIcon icon={faUtensils} className="text-muted" />
-					<span>Menu</span>
-				</DashboardButton>
-				<DashboardButton
-				// TODO: add logout functionality
-				>
-					<FontAwesomeIcon
-						icon={faArrowRightFromBracket}
-						className="text-muted"
-					/>
-					<span>Wyloguj</span>
-				</DashboardButton>
-			</div>
-		</Card>
+		<div className="flex flex-wrap gap-4 justify-center items-center w-full h-full">
+			<DashboardButton
+				onClick={() => {
+					navigate("/menu");
+				}}
+			>
+				<FontAwesomeIcon icon={faUtensils} className="text-muted" />
+				<span>Menu</span>
+			</DashboardButton>
+			<DashboardButton
+			// TODO: add logout functionality
+			>
+				<FontAwesomeIcon
+					icon={faArrowRightFromBracket}
+					className="text-muted"
+				/>
+				<span>Wyloguj</span>
+			</DashboardButton>
+		</div>
 	);
 }
