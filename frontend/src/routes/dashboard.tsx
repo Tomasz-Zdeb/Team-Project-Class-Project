f064cf9ff950db453dead95de55efd7d626ec85a
jackpie <66798628+jackpie2@users.noreply.github.com>
2023-11-17 16:55:47 +0100

    Dodaj zegar

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -6,12 +6,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
 import Card from "../components/Card";
 import DashboardButton from "../components/DashboardButton";
+import Clock from "../components/Clock";
 
 export default function Dashboard() {
 	const navigate = useNavigate();
 
 	return (
 		<Card className="h-full w-full text-3xl">
+			<div className="w-full flex justify-end">
+				<Clock className="text-muted text-4xl" />
+			</div>
 			<div className="flex flex-wrap gap-4 justify-center items-center w-full h-full">
 				<DashboardButton
 					onClick={() => {
