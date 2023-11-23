5071c1e6b4f720e427cadc0650a6d646e4d333ca
Piotr <p.zimirski@gmail.com>
2023-11-23 13:07:50 +0100

    Uzupelnij branch o zmiany w urls.py w katalogu projektu

--- backend/EaterNet/EaterNet/urls.py
+++ backend/EaterNet/EaterNet/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from app.views import api_test
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from rest_framework_simplejwt import views as jwt_views
 
 
 schema_view = get_schema_view(
@@ -36,7 +37,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_test/', api_test, name='api_test'),
+    path('', include('authentification.urls')),
+
+    #Swagger
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # Simple_JWT (tokens):
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
 ]
