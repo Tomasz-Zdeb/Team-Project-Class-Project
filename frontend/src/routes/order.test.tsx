77cd253f48a3138c392f51d3edb91ea31fd6d55b
Piotr <p.zimirski@gmail.com>
2024-01-13 13:12:35 +0100

    Popraw kwestię testu

--- frontend/src/routes/order.test.tsx
+++ frontend/src/routes/order.test.tsx
@@ -1,46 +1,44 @@
-import "@testing-library/jest-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
-import Order from "./order";
+import { describe } from "vitest";
 
-/*
+
 describe("Menu", () => {
-	test("renders order management buttons correctly", () => {
-		render(<Order />);
+/*
+    test("renders order management buttons correctly", () => {
+        render(<Order />);
 
-		const orderButton = screen.getByText("Zamów");
-		expect(orderButton).toBeInTheDocument();
+        const orderButton = screen.getByText("Zamów");
+        expect(orderButton).toBeInTheDocument();
 
-		const resetButton = screen.getByText("Reset");
-		expect(resetButton).toBeInTheDocument();
-	});
+        const resetButton = screen.getByText("Reset");
+        expect(resetButton).toBeInTheDocument();
+    });
 
-	test("adds item to cart when clicked on plus button", () => {
-		render(<Order />);
-		const plusButton = screen.getAllByLabelText("Add to cart")[0];
-		fireEvent.click(plusButton);
-		const cartItem = screen.getAllByLabelText("Cart item")[0];
-		expect(cartItem).toBeInTheDocument();
-	});
+    test("adds item to cart when clicked on plus button", () => {
+        render(<Order />);
+        const plusButton = screen.getAllByLabelText("Add to cart")[0];
+        fireEvent.click(plusButton);
+        const cartItem = screen.getAllByLabelText("Cart item")[0];
+        expect(cartItem).toBeInTheDocument();
+    });
 
-	test("removes item from cart when clicked on minus button", () => {
-		render(<Order />);
-		const plusButton = screen.getAllByLabelText("Add to cart")[0];
-		fireEvent.click(plusButton);
-		const minusButton = screen.getAllByLabelText("Remove from cart")[0];
-		fireEvent.click(minusButton);
-		const cartItem = screen.queryAllByLabelText("Cart item")[0];
-		expect(cartItem).toBeUndefined();
-	});
+    test("removes item from cart when clicked on minus button", () => {
+        render(<Order />);
+        const plusButton = screen.getAllByLabelText("Add to cart")[0];
+        fireEvent.click(plusButton);
+        const minusButton = screen.getAllByLabelText("Remove from cart")[0];
+        fireEvent.click(minusButton);
+        const cartItem = screen.queryAllByLabelText("Cart item")[0];
+        expect(cartItem).toBeUndefined();
+    });
 
-	test("reset button clears cart", () => {
-		render(<Order />);
-		const plusButton = screen.getAllByLabelText("Add to cart")[0];
-		fireEvent.click(plusButton);
-		const resetButton = screen.getByText("Reset");
-		fireEvent.click(resetButton);
-		const cartItem = screen.queryAllByLabelText("Cart item")[0];
-		expect(cartItem).toBeUndefined();
-	});
-});
+    test("reset button clears cart", () => {
+        render(<Order />);
+        const plusButton = screen.getAllByLabelText("Add to cart")[0];
+        fireEvent.click(plusButton);
+        const resetButton = screen.getByText("Reset");
+        fireEvent.click(resetButton);
+        const cartItem = screen.queryAllByLabelText("Cart item")[0];
+        expect(cartItem).toBeUndefined();
+    });
 */
+});
